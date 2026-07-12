9b9dd497832918706faa8d6a665d1790fff5c52d
leoric <rokkitsienze@gmail.com>
2026-07-13 05:59:53 +0700

    init

---
+++ data-updates.lua
@@ -0,0 +1,2 @@
+data.raw["night-vision-equipment"]["night-vision-equipment"].darkness_to_turn_on = 0.35
+data.raw["night-vision-equipment"]["night-vision-equipment"].color_lookup = {{0.5, "__faster-nightvision__/graphics/color_luts/lut-sunset-bright.png"}}
